d9d3f122a7405a70ec96b704b034b63043a2b493
JasonDeKhanh <giakhanh.lehoang@gmail.com>
2023-07-08 23:02:29 +0700

    Updated some stylings for stall list element, max width now 32% on larger screens

--- src/components/StallListEl.js
+++ src/components/StallListEl.js
@@ -12,8 +12,8 @@ function StallListEl(props) {
   return (
     <div
       onClick={handleOnClickStall}
-      className="mx-4 my-4 h-40 cursor-pointer overflow-hidden rounded-2xl bg-[#ffffcc] transition-all duration-100 ease-in hover:scale-[1.01] hover:bg-[#CCCC98] 
-      md:mx-2 md:min-w-min md:max-w-full md:shrink-0 md:grow md:basis-1/3 xl:basis-1/4"
+      className="md:max-w-content mx-4 my-4 h-40 cursor-pointer overflow-hidden rounded-2xl bg-[#ffffcc] transition-all duration-100 ease-in hover:scale-[1.01] 
+      hover:bg-[#CCCC98] md:mx-2 md:min-w-min md:max-w-[32%] md:shrink-0 md:grow md:basis-1/3 xl:basis-1/4"
     >
       <div className="mx-2 flex h-full">
         {/* Image ratio is 4x3 */}
@@ -30,7 +30,7 @@ function StallListEl(props) {
           <span className="mt-1 text-sm font-medium lg:text-base lg:font-semibold">
             Opening Hours:
             <br />
-            {stall.openingHours}
+            <p className="text-xs leading-relaxed">{stall.openingHours}</p>
           </span>
         </div>
       </div>
